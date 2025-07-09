cdcfab6e43a2e500748605ff399cf459000fce54
Isaac_VR <isaac.vera@utec.edu.pe>
2025-07-08 22:08:00 -0500

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes.recognition import router as recognition_router
+# from app.api.routes.recognition import router as recognition_router
 from app.api.routes.users import router as users_router
 from app.api.routes.logs import router as logs_router
 from app.core.config import settings
@@ -22,7 +22,7 @@ def root():
     return {"mensaje": "Bienvenido a la API de reconocimiento facial"}
 
 # Montamos el router del endpoint
-app.include_router(recognition_router, prefix="/api")
+# app.include_router(recognition_router, prefix="/api")
 app.include_router(users_router, prefix="/api")
 app.include_router(logs_router, prefix="/api")
 
